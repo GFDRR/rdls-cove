8a5e5adad5380d74ad067e2b057b7649d1323b1f
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2023-08-22 16:17:11 +0100

    cove_rdls/process.py: Fix _clean_json method

--- cove_rdls/process.py
+++ cove_rdls/process.py
@@ -127,7 +127,7 @@ class ConvertSpreadsheetIntoJSON(ProcessDataTask):
     def _clean_json(self):
         with open(os.path.join(output_dir, "unflattened.json"), "r+") as f:
             json_data = json.loads(f.read())
-            json_data = {"datasets": [for dataset in json_data if len(dataset) > 0]}
+            json_data = {"datasets": [dataset for dataset in json_data if len(dataset) > 0]}
             f.seek(0)
             f.write(json.dumps(json_data))
             f.truncate()
