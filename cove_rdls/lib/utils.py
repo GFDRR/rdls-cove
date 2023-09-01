d85b6de2ac460d4c2fa0f4905c3b41e1e5e848e3
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2023-09-01 16:40:55 +0100

    cove_rdls/lib/utils.py: Fix group_validation_errors

--- cove_rdls/lib/utils.py
+++ cove_rdls/lib/utils.py
@@ -7,7 +7,7 @@ def group_validation_errors(validation_errors):
     for key in validation_errors:
         vtype = validation_errors[key][0]["validator"]
         if vtype == "required":
-            validation_errors_grouped["required"].append(validation_errors[key])
+            validation_errors_grouped["required"].append({key: validation_errors[key]})
         elif vtype in [
             "format",
             "pattern",
@@ -19,7 +19,7 @@ def group_validation_errors(validation_errors):
             "integer",
             "array",
         ]:
-            validation_errors_grouped["format"].append(validation_errors[key])
+            validation_errors_grouped["format"].extend({key: validation_errors[key]})
         else:
-            validation_errors_grouped["other"].append(validation_errors[key])
+            validation_errors_grouped["other"].append({key: validation_errors[key]})
     return validation_errors_grouped
