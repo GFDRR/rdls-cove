6377b3d49590da4f013212371b13ea4a4b910cd6
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2023-09-04 09:36:49 +0100

    cove_rdls/templatetags/validator_table.py: Add template tags

--- cove_rdls/templatetags/validator_table.py
+++ cove_rdls/templatetags/validator_table.py
@@ -8,3 +8,19 @@ register = template.Library()
 @stringfilter
 def property_name(value):
     return value.split("/")[0]
+
+
+@register.filter
+def start_end(value):
+    if isinstance(value, list):
+        if value[-1] == "start" or value[-1] == "end":
+            return True
+    return False
+
+
+@register.filter
+def links_rel(value):
+    if isinstance(value, list):
+        if len(value) > 1 and "links" in value and value[-1] == "rel":
+            return True
+    return False
