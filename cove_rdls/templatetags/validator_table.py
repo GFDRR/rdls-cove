11f5383e573ec5f119691169825c1901a66203de
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2023-09-06 22:28:56 +0100

    cove_rdls/templatetags/validator_table.py: Fix rel error

--- cove_rdls/templatetags/validator_table.py
+++ cove_rdls/templatetags/validator_table.py
@@ -21,7 +21,7 @@ def temporal_start_end(value):
 @register.filter
 def links_rel_first(value):
     if isinstance(value, list):
-        if len(value) > 1 and "links" in value and value[-2] == "0" and value[-1] == "rel":
+        if len(value) > 1 and "links" in value and value[-2] == 0 and value[-1] == "rel":
             return True
     return False
 
@@ -29,6 +29,6 @@ def links_rel_first(value):
 @register.filter
 def links_rel_subsequent(value):
     if isinstance(value, list):
-        if len(value) > 1 and "links" in value and value[-2] != "0" and value[-1] == "rel":
+        if len(value) > 1 and "links" in value and value[-2] != 0 and value[-1] == "rel":
             return True
     return False
